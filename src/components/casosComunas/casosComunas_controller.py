86e93a1a60fe6b66854dee3bb2fa56c197a774db
Menitox <pablocontrerasochoa@gmail.com>
2021-05-19 22:47:45 -0400

    main methods for casosComunas finished

--- src/components/casosComunas/casosComunas_controller.py
+++ src/components/casosComunas/casosComunas_controller.py
@@ -13,7 +13,7 @@ def init(connection):
                 firstLine = False
                 continue
             NOMBRE, CODIGO, POBLACION, CASOS = i.split(",")
-            casoComuna = CasoComuna(NOMBRE, CODIGO, CASOS)
+            casoComuna = CasoComuna(NOMBRE, CODIGO, POBLACION, CASOS)
             repository.post(casoComuna, cursor)
     except Exception as error:
         print("No se pudo inicializar CASOS_POR_COMUNA en base al csv: ", error)
@@ -24,6 +24,20 @@ def init(connection):
         file.close()
         cursor.close()
         return
+
+def get(id : str, connection):
+    cursor = connection.cursor()
+    casoComuna : CasoComuna = None
+    try:
+        casoComuna = repository.get(id, cursor)
+    except Exception as error:
+        print("Error GET a CASOS_POR_COMUNA: ", error)
+        return 
+    else:
+        print("GET exitoso a CASOS_POR_COMUNA")
+    finally:
+        cursor.close()
+        return casoComuna
 
 def post(casoComuna : CasoComuna, connection):
     cursor = connection.cursor()
@@ -51,5 +65,16 @@ def delete(id : str, connection):
         cursor.close()
         return
 
-def patch(casoComuna : CasoComuna, connection):
-    pass
+def patch(id :str, casoComuna : list(), connection):
+    cursor = connection.cursor()
+    casoComuna = CasoComuna(casoComuna[0],casoComuna[1],casoComuna[2],casoComuna[3])
+    try:
+        repository.patch(id, casoComuna, cursor)
+    except Exception as error:
+        print("Error PATCH a CASOS_POR_COMUNA: ", error)
+    else:
+        print("PATCH exitoso a CASOS_POR_COMUNA")
+        connection.commit()
+    finally:
+        cursor.close()
+        return
--- src/components/casosComunas/casosComunas_repository.py
+++ src/components/casosComunas/casosComunas_repository.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('src/models')
+
 from casosComunas_model import CasoComuna
 
 def init(cursor):
@@ -6,6 +9,7 @@ def init(cursor):
             CREATE TABLE CASOS_POR_COMUNA(
                 COMUNA VARCHAR2(50) NOT NULL,
                 CODIGO_DE_COMUNA VARCHAR2(50) NOT NULL,
+                POBLACION NUMBER NOT NULL,
                 CASOS_CONFIRMADOS NUMBER NOT NULL,
                 PRIMARY KEY(CODIGO_DE_COMUNA)
             )
@@ -14,14 +18,52 @@ def init(cursor):
     print("Creada primera instancia de CASOS_POR_COMUNA")
     return
 
+def get(id : str, cursor):
+    cursor.execute(
+        """
+            SELECT *
+            FROM CASOS_POR_COMUNA
+            WHERE CODIGO_DE_COMUNA = :id
+        """
+        ,[id] 
+        )
+    for COMUNA, CODIGO_DE_COMUNA, POBLACION, CASOS_CONFIRMADOS in cursor:
+        casoComuna = CasoComuna(COMUNA, CODIGO_DE_COMUNA, POBLACION, CASOS_CONFIRMADOS)
+    return casoComuna
+
 def post(casoComuna : CasoComuna, cursor):
     cursor.execute (
         """
         INSERT INTO
-            CASOS_POR_COMUNA(COMUNA, CODIGO_DE_COMUNA, CASOS_CONFIRMADOS)
+            CASOS_POR_COMUNA(COMUNA, CODIGO_DE_COMUNA, POBLACION, CASOS_CONFIRMADOS)
         VALUES
-            (:1, :2, :3)
+            (:1, :2, :3, :4)
         """
-        , (casoComuna.nombre, casoComuna.codigo, casoComuna.casos)
+        , (casoComuna.nombre, casoComuna.codigo, casoComuna.poblacion, casoComuna.casos)
+    )
+    return
+
+def delete(id : str, cursor):
+    cursor.execute(
+        """
+        DELETE FROM CASOS_POR_COMUNA WHERE CODIGO_DE_COMUNA = :id
+        """
+        ,[id]
+    )
+    return
+
+def patch(id : str, casoComuna : CasoComuna, cursor):
+    cursor.execute(
+        """
+        UPDATE CASOS_POR_COMUNA
+        SET
+            COMUNA = NVL(:1, COMUNA),
+            POBLACION = NVL(:2, POBLACION),
+            CODIGO_DE_COMUNA = NVL(:3, CODIGO_DE_COMUNA),
+            CASOS_CONFIRMADOS = NVL(:4, CASOS_CONFIRMADOS)
+        WHERE
+            CODIGO_DE_COMUNA = :5
+        """
+        ,(casoComuna.nombre, casoComuna.poblacion, casoComuna.codigo, casoComuna.casos, id)
     )
     return
